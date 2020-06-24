a346ae1aa1e5d9b8065dab4be2399882f32b8e8c
sandrofrmn <sandro.offermans@icloud.com>
2020-06-24 15:19:55 +0200

    initial version

---
+++ Ziekenhuis_StayHealthy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ziekenhuis_StayHealthy.MvcApplication" Language="C#" %>
---
+++ Ziekenhuis_StayHealthy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ziekenhuis_StayHealthy.Startup))]
+namespace Ziekenhuis_StayHealthy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
